a2c22669d5de5b8fcca4f393e1b2b8113eb1fe84
devopsRP <krpofficial25@gmail.com>
2026-03-20 00:26:39 +0530

    storage account

---
+++ storageAccount/main.tf
@@ -0,0 +1,21 @@
+#Resource group block
+
+resource "azurerm_resource_group" "Rp-rgroup" {
+  name     = "storage-resources"
+  location = "Central India"
+}
+
+
+#storage account block
+
+resource "azurerm_storage_account" "storageaccount" {
+  name                     = "rpstorageaccountname"
+  resource_group_name      = azurerm_resource_group.Rp-rgroup.name
+  location                 = azurerm_resource_group.Rp-rgroup.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
